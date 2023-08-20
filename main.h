a151db0010cfad6db31714d4ebf50d13aad3087b
kwameadobaw <kwame.adobawjnr@gmail.com>
2023-08-20 12:06:54 -0700

    Testing task 1

--- main.h
+++ main.h
@@ -8,5 +8,10 @@
 
 int _putchar(char c);
 int _printf(const char *format, ...);
+int _abs(int n);
+int _strlen(char *s);
+void _reverse(char *s);
+void _itoa(int n, char *s);
+void _print_int(int n);
 
 #endif

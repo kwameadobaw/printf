9dab84248f9ca74ff2946269838b8d0e52e8b4b5
Dr. Appiagyei <ohenebappiagyei@gmail.com>
2023-08-21 12:31:15 -0700

    finished at 7:31

--- 1-printf.c
+++ 1-printf.c
@@ -1,29 +1,35 @@
 #include <stdarg.h>
 #include "main.h"
+
 /**
  * print_char - Print a character
  * @args: Arguments list
  *
  * Return: Number of characters printed
- */
+*/
 int print_char(va_list args)
 {
-	char c = va_arg(args, int);
+	char c;
+
+	va_arg(args, int);
 
 	_putchar(c);
 	return (1);
 }
 
 /**
- * print_string - Print a string
+ * print_string - Prints a string
  * @args: Arguments list
  *
  * Return: Number of characters printed
- */
+*/
 int print_string(va_list args)
 {
-	char *str = va_arg(args, char *);
-	int count = 0;
+	char *str;
+	int count;
+
+	str = va_arg(args, char *);
+	count = 0;
 
 	if (!str)
 		str = "(null)";
@@ -37,14 +43,47 @@ int print_string(va_list args)
 }
 
 /**
- * print_percent - Print a percent character
+ * print_int - Print an integer
+ * @args: Arguments list
  *
  * Return: Number of characters printed
- */
-int print_percent(void)
+*/
+int print_int(va_list args)
 {
-	_putchar('%');
-	return (1);
+	int num, int count, num_digits, temp, num_digits, temp, divisor;
+
+	num = va_arg(args, int);
+	count = 0;
+
+	if (num < 0)
+	{
+		_putchar('-');
+		count++;
+		num = -num;
+	}
+
+	num_digits = 0;
+	temp = num;
+	do {
+		num_digits++;
+		temp /= 10;
+	} while (temp != 0);
+
+	divisor = 1;
+	for (int i = 1; i < num_digits; i++)
+		divisor *= 10;
+
+	while (divisor > 0)
+	{
+		int digit = num / divisor;
+
+		_putchar('0' + digit);
+		count++;
+		num %= divisor;
+		divisor /= 10;
+	}
+
+	return (count);
 }
 
 /**
@@ -53,10 +92,10 @@ int print_percent(void)
  * @...: Additional arguments based on format
  *
  * Return: Number of characters printed
- */
+*/
 int _printf(const char *format, ...)
 {
-	int count = 0;
+	int count;
 	va_list args;
 
 	va_start(args, format);
@@ -70,16 +109,7 @@ int _printf(const char *format, ...)
 			if (*format == '\0')
 				return (-1);
 			if (*format == 'c')
-				count += print_char(args);
-			else if (*format == 's')
-				count += print_string(args);
-			else if (*format == '%')
-				count += print_percent();
-		}
-		else
-		{
-			_putchar(*format);
-			count++;
+				count++;
 		}
 		format++;
 	}
--- README.md
+++
@@ -1,1 +0,0 @@
-# this is The printf project

4892344c6e6fde3fe493e3ba3cec427d8e992b77
kwameadobaw <kwame.adobawjnr@gmail.com>
2023-08-21 15:20:26 -0700

    Task 0

--- main.h
+++ main.h
@@ -9,5 +9,8 @@
 int _putchar(char c);
 int print_int(va_list args);
 int _printf(const char *format, ...);
+int print_char(va_list args);
+int print_string(va_list args);
+int print_percent(void);
 
 #endif

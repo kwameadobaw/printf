e3c6ac3a51fa6186d9d8ca452769c4b58ebc7bdd
kwameadobaw <kwame.adobawjnr@gmail.com>
2023-08-21 01:46:45 -0700

    New task 0

--- 0-printf.c
+++ 0-printf.c
@@ -1,22 +1,64 @@
-#include <stdarg.h>
-#include <unistd.h>
 #include "main.h"
 /**
- * _printf - Custom implementation of printf
- * @format: Format string
- * @:...: Variable number of arguments
+ * print_char - Print a character
+ * @args: Arguments list
+ *
  * Return: Number of characters printed
-*/
+ */
+int print_char(va_list args)
+{
+	char c = va_arg(args, int);
+
+	_putchar(c);
+	return (1);
+}
+
+/**
+ * print_string - Print a string
+ * @args: Arguments list
+ *
+ * Return: Number of characters printed
+ */
+int print_string(va_list args)
+{
+	char *str = va_arg(args, char *);
+	int count = 0;
+
+	if (!str)
+		str = "(null)";
+	while (*str)
+	{
+		_putchar(*str);
+		str++;
+		count++;
+	}
+	return (count);
+}
+
+/**
+ * print_percent - Print a percent character
+ *
+ * Return: Number of characters printed
+ */
+int print_percent(void)
+{
+	_putchar('%');
+	return (1);
+}
+
+/**
+ * _printf - Custom printf function
+ * @format: The format string
+ * @...: Additional arguments based on format
+ *
+ * Return: Number of characters printed
+ */
 int _printf(const char *format, ...)
 {
-	int count;
-	char *str;
+	int count = 0;
 	va_list args;
-	int c;
 
 	va_start(args, format);
-	count = 0;
-
 	if (!format)
 		return (-1);
 	while (*format)
@@ -27,28 +69,11 @@ int _printf(const char *format, ...)
 			if (*format == '\0')
 				return (-1);
 			if (*format == 'c')
-			{
-				c = va_arg(args, int);
-				_putchar(c);
-				count++;
-			}
+				count += print_char(args);
 			else if (*format == 's')
-			{
-				str = va_arg(args, char *);
-				if (!str)
-					str = "(null)";
-				while (*str)
-				{
-					_putchar(*str);
-					str++;
-					count++;
-				}
-			}
+				count += print_string(args);
 			else if (*format == '%')
-			{
-				_putchar('%');
-				count++;
-			}
+				count += print_percent();
 		}
 		else
 		{
--- main.h
+++ main.h
@@ -5,13 +5,12 @@
 #include <stdarg.h>
 #include <stdlib.h>
 #include <unistd.h>
+#include <string.h>
 
 int _putchar(char c);
 int _printf(const char *format, ...);
-int _abs(int n);
-int _strlen(char *s);
-void _reverse(char *s);
-void _itoa(int n, char *s);
-int _print(const char *format, ...);
+int print_char(va_list args);
+int print_string(va_list args);
+int print_percent(void);
 
 #endif

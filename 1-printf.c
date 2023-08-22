0936671409428e654574f37e90b5955c8d92621e
kwameadobaw <kwame.adobawjnr@gmail.com>
2023-08-21 17:30:28 -0700

    Updated

--- 1-printf.c
+++ 1-printf.c
@@ -8,32 +8,25 @@
 int _printf(const char *format, ...)
 {
 	va_list args;
-	int count;
+	int count, num;
 
 	va_start(args, format);
 	count = 0;
 
 	while (*format != '\0')
 	{
-		va_list args;
-		int count;
-
-		va_start(args, format);
-		count = 0;
-
-		while (*format != '\0')
+		if (*format == '%')
 		{
+			format++;
+			if (*format == '\0')
+				break;
 			if (*format == '%')
 			{
-				format++;
-				if (*format == '\0')
-					break;
-				if (*format == '%');
+				_putchar('%');
 				count++;
 			}
 			else if (*format == 'd' || *format == 'i')
 			{
-				int num;
 
 				num = va_arg(args, int);
 			}
@@ -50,5 +43,8 @@ int _printf(const char *format, ...)
 			count++;
 		}
 		format++;
-		va_end(args);
-		return (count);
+	}
+	
+	va_end(args);
+	return (count);
+}

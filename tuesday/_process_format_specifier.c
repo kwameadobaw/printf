945376244b291caf3b6a3a02d710bc010be86408
Dr. Appiagyei <ohenebappiagyei@gmail.com>
2023-08-24 00:12:30 -0700

    FORMAT SPECIFIER UPDATED

--- tuesday/_process_format_specifier.c
+++ tuesday/_process_format_specifier.c
@@ -5,7 +5,8 @@
  * @buffer: The buffer to store the formatted output
  * @remaining_size: Remaining space in the buffer
  * @args: The arguments to be formatted
- * printed_Pointer to the number of printed characters
+ * @printed_chars: Pointer to the number of printed characters
+ * @print_char: Pointer to the function that prints a single character
  * Return: The number of characters written to the buffer
 */
 int _process_format_specifier_char(const char **format,
@@ -32,15 +33,16 @@ int _process_format_specifier_char(const char **format,
 
 	(*format)++;
 	return (chars_written);
+}
 	
-#include "main.h"
 /**
  * _process_format_specifier_string - Process format specifier
  * @format: Pointer to the format string
  * @buffer: The buffer to store the formatted output
  * @remaining_size: Remaining space in the buffer
  * @args: The arguments to be formatted
- * printed_Pointer to the number of printed characters
+ * @printed_chars: Pointer to the number of printed characters
+ * @print_char: Pointer to a function that prints a single character
  * Return: The number of characters written to the buffer
 */
 int _process_format_specifier_string(const char **format,
@@ -62,15 +64,16 @@ int _process_format_specifier_string(const char **format,
 
 	(*format)++;
 	return (chars_written);
+}
 	
-#include "main.h"
 /**
  * _process_format_specifier_number - Process format specifier
  * @format: Pointer to the format string
  * @buffer: The buffer to store the formatted output
  * @remaining_size: Remaining space in the buffer
  * @args: The arguments to be formatted
- * printed_Pointer to the number of printed characters
+ * @printed_chars: Pointer to the number of printed characters
+ * @print_chars: Pointer to the function that prints a single character
  * Return: The number of characters written to the buffer
 */
 int _process_format_specifier_char(const char **format,
@@ -98,3 +101,45 @@ int _process_format_specifier_char(const char **format,
 	(*format)++;
 	return (chars_written);
 }
+/**
+ * _process_format_specifier_u_o - Process format specifier
+ * @format: Pointer to the format string
+ * @buffer: The buffer to store the formatted output.
+ * @remaining_size: Remaining space in the buffer
+ * @args: THe arguments to be formatted.
+ * @printed_chars: Pointner to the number of printed characters.
+ * @print_char: A pointer to a function that prints a single character
+ *
+ * Return: The number of characters written to the buffer.
+*/
+int _Process_format_specifier_u_o(const char **format,
+		char *buffer,
+		size_t remaining_size,
+		va_list args,
+		int *printed_chars,
+		void (*print_char)(char *buffer, int character))
+{
+	int chars_written, base, uppercase;
+
+	chars_written = 0;
+
+	if (**format = 'u' || *formatt == 'o')
+	{
+		base = (**format == 'o') ? 8 : 10;
+		uppercase = 0;
+		(*format)++;
+
+		chars_written += _print_unsigned(va_arg(args, unsigned int), base,
+				uppercase, buffer + chars_written,
+				remaining_size - chars_written, print_char);
+		(*printed_chars) += chars_written;
+	}
+
+	(*format)++;
+	return (chars_written);
+}
+/**
+ * _handle_S-specifier - Handles the custom S specifier
+ * @format: Pointer to the format string
+ * @buffer: The buffer to store the formatted output
+ * @remaining_size
